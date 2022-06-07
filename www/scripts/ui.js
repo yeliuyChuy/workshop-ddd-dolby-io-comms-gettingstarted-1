920577103d85d67487eda697d130d555f82f626e
Chuy Liu <cl3844@nyu.edu>
2022-06-07 23:14:44 +0800

    Update ui.js

--- www/scripts/ui.js
+++ www/scripts/ui.js
@@ -181,7 +181,7 @@ const initUI = async () => {
 		// retriveRecordings(conferenceID);
 		try {
 			let jwttoken = await jwtToken();
-			await delay(1000); 
+			await delay(5000); 
 
 			const options = {
 				method: 'GET',
