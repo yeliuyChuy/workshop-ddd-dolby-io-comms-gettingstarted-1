3a2560ae8ca66470ea2c0b9841d327c6679a5937
Chuy Liu <cl3844@nyu.edu>
2022-06-08 14:03:31 +0800

    Update ui.js

--- www/scripts/ui.js
+++ www/scripts/ui.js
@@ -180,11 +180,7 @@ const initUI = async () => {
 		let conferenceID = VoxeetSDK.conference.current.id;
 		let participants = VoxeetSDK.conference.participants;
 
-		console.log(participants);
-		for (let i in participants) {
-			console.log("----------");
-			console.log(i);
-		}
+		console.log(participants.length);
 
 		VoxeetSDK.conference
 			.leave()
