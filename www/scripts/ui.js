146b2c9b3f5a2a29a8bfd69119fb35c534899672
Chuy Liu <cl3844@nyu.edu>
2022-06-08 12:35:38 +0800

    Update ui.js

--- www/scripts/ui.js
+++ www/scripts/ui.js
@@ -929,8 +929,8 @@ async function checkIfRecordingsAvailable(conferenceID) {
 		checkIfRecordingsAvailable(conferenceID);
 	} else {
 		console.log("Recordings are available now !");
-		console.log("========");
-		const recording = result.records[0];
+		console.log("++++++++++");
+		const recording = result.records[0].splits;
 		// console.log(result);
 		console.log(recording);
 		return result;
